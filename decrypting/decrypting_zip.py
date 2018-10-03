3dc5bec32f3a61f9fa19bc0c72c1539ddbda89ee
rjunior8 <r_junior@protonmail.com>
2018-10-03 20:16:13 -0300

    Octuber - 3rd - 2018

--- decrypting/decrypting_zip.py
+++ decrypting/decrypting_zip.py
@@ -7,7 +7,7 @@ for line in pass_file.readlines():
   password = line.strip("\n")
 
   try:
-    zf.extractall(pwd=password.encode("cp850", "replace"))
+    zf.extractall(path="~/test3/", pwd=password.encode("cp850", "replace"))
     print("\nPassword Found: {}\n".format(password))
     exit(0)
   except Exception as e:
